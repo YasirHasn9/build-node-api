e054bb6c3dd067dcc4241a3ad6c7e4b81bbbcdc1
Yasir <hasanyasir01@gmail.com>
2022-02-09 12:46:55 -0800

    test server on the local host

---
+++ index.js
@@ -0,0 +1,15 @@
+// 1.initialize our app
+const app = require("express");
+// 2.create server
+const server = app();
+
+// 4.create our endpoints
+server.get("/", (req, res) => {
+  res.json("Hello world");
+});
+
+// 3.create a local host
+// listen is a built-in function that established a connection on a specific host or port
+// it takes 2 parameters, the first one the host number and the
+// second is an optional callback function
+server.listen(4000, () => console.log(`http://localhost:${4000}`));
